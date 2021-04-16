4dbf98287e6fe395f8c47351fd06de8777c43166
Mateusz Chrusciel <tiskus.3009@gmail.com>
2021-04-16 20:02:39 +0200

    serialization should be working for now, unused size in serialize, need tests

--- src/lindaTuples.cpp
+++ src/lindaTuples.cpp
@@ -1,24 +1,19 @@
 #include "lindaTuples.h"
-
+#define MAX_TUPLE_SIZE 128
+#define MAX_TUPLE_NUMBER 10
 std::optional<std::list<Tuple>::iterator>
-
-LindaTuples::match(const RegexTuple &tuple)
-{
+LindaTuples::match(const RegexTuple &tuple) {
   bool found = true;
-  for (auto it = tuples_.begin(); it != tuples_.end(); ++it)
-  {
+  for (auto it = tuples_.begin(); it != tuples_.end(); ++it) {
     if (!((*it).size() == tuple.size()))
       continue;
     found = true;
-    for (size_t i = 0; i < (*it).size(); ++i)
-    {
-      if ((*it)[i].index() != tuple[i].value_.index())
-      {
+    for (size_t i = 0; i < (*it).size(); ++i) {
+      if ((*it)[i].index() != tuple[i].value_.index()) {
         found = false;
         break;
       }
-      switch (tuple[i].operator_)
-      {
+      switch (tuple[i].operator_) {
       case EQ:
         if (!((*it)[i] == tuple[i].value_))
           found = false;
@@ -51,27 +46,24 @@ LindaTuples::match(const RegexTuple &tuple)
   return {};
 }
 
-bool LindaTuples::output(const Tuple &tuple)
-{
+bool LindaTuples::output(const Tuple &tuple) {
+  if (tuple.size() == 0)
+    throw std::invalid_argument("CANNOT WRITE EMPTY TUPLE!");
   tuples_.push_back(tuple);
   return true;
 }
 
-Tuple LindaTuples::input(const RegexTuple &tuple)
-{
+Tuple LindaTuples::input(const RegexTuple &tuple) {
   auto iter = match(tuple);
-  if (iter.has_value())
-  {
+  if (iter.has_value()) {
     Tuple ret_tuple = *iter.value();
     tuples_.erase(iter.value());
     return ret_tuple;
-  }
-  else
+  } else
     return Tuple{"waiting"};
 }
 
-Tuple LindaTuples::read(const RegexTuple &tuple)
-{
+Tuple LindaTuples::read(const RegexTuple &tuple) {
   auto ret = match(tuple);
   if (ret.has_value())
     return *ret.value();
@@ -79,31 +71,70 @@ Tuple LindaTuples::read(const RegexTuple &tuple)
     return Tuple{"waiting"};
 }
 
-size_t LindaTuples::getTuplesAmount()
-{
-  return tuples_.size();
-}
+size_t LindaTuples::getTuplesAmount() { return tuples_.size(); }
 
-bool LindaTuples::deserialize(const char *address)
-{
+bool LindaTuples::deserialize(const char *address) {
+  tuples_.clear();
+  std::variant<std::string, int, float> value;
+  char single_tuple[MAX_TUPLE_SIZE];
+  for (int i = 0; i < MAX_TUPLE_NUMBER; ++i) {
+    memset(single_tuple, 0, sizeof(single_tuple));
+    strcpy(single_tuple, address + i * MAX_TUPLE_SIZE);
+
+    std::string str(single_tuple);
+
+    // end if all there are less tha max tuples
+    if (str.empty())
+      return true;
+
+    std::cout << "PRZECZYTANO: " << str << std::endl;
+    Tuple desirialized_tuple;
+
+    std::regex parse_types(":");
+    std::smatch base_match;
+
+    std::string delimiter = ";";
+    size_t pos = 0;
+    std::string token;
+
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+      token = str.substr(0, pos);
+      str.erase(0, pos + delimiter.length());
+
+      std::regex_search(token, base_match, parse_types);
+      std::string type_s = base_match.prefix();
+      std::string value_s = base_match.suffix();
+      if (type_s == "0")
+        value = value_s;
+      else if (type_s == "1")
+        value = stoi(value_s);
+      else if (type_s == "2")
+        value = stof(value_s);
+      desirialized_tuple.push_back(value);
+    }
+    printTuple(desirialized_tuple);
+    tuples_.push_back(desirialized_tuple);
+  }
   return true;
 }
 
-bool LindaTuples::serialize(const char *address, size_t max_size)
-{
-  std::stringstream serialized_stream;
-  for (const auto &t : tuples_)
-  {
-    serialized_stream << t.size() << ':';
-    for (const auto &v : t)
-    {
-      serialized_stream << v.index() << ':' << v;
+bool LindaTuples::serialize(char *const data, size_t max_size) {
+  int offset = 0;
+  for (const auto &t : tuples_) {
+    std::stringstream serialized_stream;
+    for (const auto &v : t) {
+      serialized_stream << v.index() << ':' << v << ';';
     }
     std::string serialized_tuple = serialized_stream.str();
-    if (serialized_tuple.size() >= 512)
+    if (serialized_tuple.size() >= MAX_TUPLE_SIZE)
       throw(std::invalid_argument("MAX TUPLE SIZE IS EXCEEDED"));
 
-    // write to shared memory
+    char tuple_char[MAX_TUPLE_SIZE];
+    memset(tuple_char, 0, sizeof(tuple_char));
+    strncpy(tuple_char, serialized_tuple.c_str(), serialized_tuple.size());
+    std::cout << tuple_char << std::endl;
+    strcpy(data + offset, tuple_char);
+    offset += MAX_TUPLE_SIZE;
   }
   return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include "lindaTuples.h"
 
 int main() {
-    std::cout<<"UWAGA"<<std::endl;
+  try {
+    char data[10 * 128];
+    memset(data, 0, sizeof(data));
+    LindaTuples l;
+    l.output({0, 1.f, "kupa", -2});
+    l.output({"dupa", -2, "papuga"});
+    // l.output({});
+    l.output({""});
+    l.serialize(data, 0);
+    l.deserialize(data);
+    std::cout << std::endl << (int)l.getTuplesAmount() << std::endl;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 }
